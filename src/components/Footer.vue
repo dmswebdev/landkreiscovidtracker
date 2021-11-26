b62fba95a7e487ecc93eca2e32ebc3cc2c6b3ace
David Manuel Schröder <hello@dmsweb.dev>
2021-11-26 12:12:06 +0100

    update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
 	<footer class="mt-20 text-center p-5 text-gray-500">
 		<p class="text-sm mb-3"><router-link to="/impressum" class="underline hover:no-underline">Impressum</router-link></p>
-		<p class="text-xs">Built by <a href="https://dmsweb.dev" target="_blank" class="underline hover:no-underline">DMS web development</a> using VueJS and <a href="https://api.corona-zahlen.org/" target="_blank" class="underline hover:no-underline">Robert Koch-Institut API</a></p>
+		<p class="text-xs">Realisiert von <a href="https://manuelschroeder.dev" target="_blank" class="underline hover:no-underline">Manuel Schröder web solutions</a> unter Verwendung von Vue.js und der <a href="https://api.corona-zahlen.org/" target="_blank" class="underline hover:no-underline">Robert Koch-Institut API</a></p>
 	</footer>
 </template>
